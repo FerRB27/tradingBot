f9bf3374b75b0134860de4f1caa0adec3aca0f51
FerRb <fernando.rodriguez@ansp.edu.sv>
2026-01-26 08:20:55 -0600

    feat: Add secrets_Test.py for testnet API keys and configuration

---
+++ config/secrets_Test.py
@@ -0,0 +1,7 @@
+# API KEY / SECRET (NO subir a Git)
+# TESTNET Keys (Dinero Virtual)
+#-------------------------------------------------------------------------------------#
+# para pruebas en testnet.
+#API_KEY = "Tqdo35u46dui4q1jWHl5UsdE5eexHtyn3TukNOVqeC4cBzrfgRx3enmmGHV9mmGo"
+#API_SECRET = "JjGT8CoiTLeGHwaH7vVLFjakwFi8wcz9ykqUzp83d9zsZAQq8Sm6QDr8appY47er"
+#-------------------------------------------------------------------------------------#
